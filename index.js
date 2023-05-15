c9b9932c502ce02227d90887855d3f265810b44c
Anali Estrada Sandoval <analiestrada44@gmail.com>
2023-05-14 20:33:20 -0700

    Sending data fetch lab completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email){
+const userData = {
+    name,
+    email
+}
+const url = "http://localhost:3000/users"
+const configurationObject = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userData),
+    };
+    return fetch(url, configurationObject)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (object){
+        document.body.append(object.id)
+    })
+    .catch(function (error){
+        alert.apply("Unathorized Access");
+        document.body.append(error.message)
+    })
+}
